ccb5095ec0c1bc56f10d155284de0eedf3fbd8d0
Md.Sakib Hossain <86706077+sakib236160@users.noreply.github.com>
2024-11-02 22:32:37 -0700

    task01 done

---
+++ task01.js
@@ -0,0 +1,20 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+let burgerPrice = 550; 
+
+// if(burgerPrice < 500){
+//     console.log('free coke paba');
+// }
+// else{
+//     console.log('coke:30tk');
+// }
+
+// *************  Here’s the "Free Drinks" rule using a ternary operator  **********************
+
+burgerPrice > 500 ? console.log('free coke paba') : console.log('coke:30tk');
